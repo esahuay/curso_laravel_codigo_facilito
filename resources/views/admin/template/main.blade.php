c114cd458f648f1bc28711a948fc0f50bf473f8e
Vaniusa <ionsaitan1980@gmail.com>
2017-02-14 00:47:33 +0100

    27

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>@yield('title', 'Default') | Panel de administracion</title>
 	<link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/chosen/chosen.css') }}">
 </head>
 <body class="admin-body">
 
@@ -35,5 +36,9 @@
 
       <script src="{{ asset('plugins/jquery/js/jquery-3.1.1.js') }}"></script>
       <script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
+      <script src="{{ asset('plugins/chosen/chosen.jquery.js') }}"></script>
+
+      @yield('js')
+
 </body>
 </html>
